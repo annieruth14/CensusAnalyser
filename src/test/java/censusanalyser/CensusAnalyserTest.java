98683474b818228a102e4f09f6d733d09c984bd9
annieruth14 <ruthannie3@gmail.com>
2020-10-19 12:34:59 +0530

    [Annie]Added: test case to check if there is a runtime exception

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -8,6 +8,7 @@ import org.junit.rules.ExpectedException;
 public class CensusAnalyserTest {
 
 	private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+	private static final String WRONG_PATH = "./src/test/resources/WrongIndiaStateCensusData.csv";
 	private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
 	private static final String INDIA_STATE_CSV = "./src/test/resources/IndiaStateCode.csv";
 	private static final String WRONG_FILE_TYPE = "./src/test/resources/IndiaStateCode.txt";
@@ -44,6 +45,15 @@ public class CensusAnalyserTest {
 			censusAnalyser.loadIndiaCensusData(WRONG_FILE_TYPE);
 		} catch (CensusAnalyserException e) {
 			Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+		}
+	}
+	
+	@Test
+	public void givenIndiaCensusData_WithIncorrectDelimiter_ShouldThrowException() {
+		try {
+			censusAnalyser.loadIndiaCensusData(WRONG_PATH);
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.MISMATCH, e.type);
 		}
 	}
 }
