d856cfbe4844b9fc0c2cb32463f06f524b79d322
annieruth14 <ruthannie3@gmail.com>
2020-10-21 22:14:36 +0530

    [Annie]Refactored: method for using CommonCSV

--- src/main/java/censusanalyser/CensusAnalyserCommonCSV.java
+++ src/main/java/censusanalyser/CensusAnalyserCommonCSV.java
@@ -5,28 +5,24 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
-
-import OpenCSV.CSVException;
-import OpenCSV.ICSVBuilder;
+import org.apache.commons.csv.CSVRecord;
 
 public class CensusAnalyserCommonCSV {
 	public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
 		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
-				CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);) 
+				CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
+						.withFirstRecordAsHeader());) 
 		{
-			ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-			Iterator<IndiaCensusCSV> censusCSVIterator = csvBuilder.getCSVFileIterator(reader, IndiaCensusCSV.class);
-			return CensusAnalyser.getCount(censusCSVIterator);
+			List<CSVRecord> csvRecords = csvParser.getRecords();
+			return csvRecords.size();
 		} 
 		catch (IOException e) {
 			throw new CensusAnalyserException(e.getMessage(),
 					CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
-		}
-		catch (CSVException e) {
-			throw new CensusAnalyserException(e.getMessage(), e.type.name());
 		}
 	}
 }
